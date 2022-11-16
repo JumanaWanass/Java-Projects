b223185eb8af796874d9bd41a623a79f471f69b4
Jumana Wanass <Jumana.wanas@gmail.com>
2022-11-17 01:07:31 +0200

    Add files via upload

---
+++ Paint/Backend/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.text.Position;
+import java.awt.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        int y = 0;
+
+
+
+
+
+
+
+    }
+
+}

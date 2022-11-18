4c3c7344f43d128e9acaf77dcc53755bd66b5913
NourMohd <116981989+NourMohd@users.noreply.github.com>
2022-11-19 01:19:33 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.lab7_minipaint;
+
+import javax.swing.text.Position;
+import java.awt.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+      
+    }
+
+}
